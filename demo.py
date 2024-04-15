892ab1792616e43ac1152bae6bf60bbafd333648
Devendrasingh Solanki <dsoulanki49@gmail.com>
2024-04-15 22:02:56 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,6 @@
+from US_visa_project.pipeline.training_pipeline import TrainPipeline
+
+pipline= TrainPipeline()
+pipline.run_pipeline()
+
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_visa_project",
+    version="0.0.0",
+    author="Rancho",
+    author_email="dsoulanki49@gmail.com",
+    packages=find_packages()
+)
